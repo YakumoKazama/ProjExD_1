c624ab75f66704d0a36fd4e05798c4d9e4674169
YakumoKazama <c0a2406442@edu.teu.ac.jp>
2025-06-10 15:03:29 +0900

    第4問.羽ばたくこうかとんを表示.

--- pygame_image.py
+++ pygame_image.py
@@ -6,6 +6,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
+    #ゲームの初期化
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
@@ -14,11 +15,13 @@ def main():
     kk_img_fly = pg.transform.flip(kk_img_fly, True, False) #左右反転
 
     tmr = 0
+    #ゲームのループ
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img_fly, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
